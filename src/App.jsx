fe008b3928d70b4704985d1e686b3d9fa9faa21d
joyshaha <eee.joy.ckz@gmail.com>
2025-11-19 16:14:22 +0600

    add: imporvement

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import "./App.css";
 import { RouterProvider } from "react-router";
 import router from "@/routes";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    const auth = localStorage.getItem("auth");
+    if (auth) {
+      router.navigate("/");
+    }
+  }, []);
   return (
     <RouterProvider router={router} />
   );
--- src/components/UserMenu.jsx
+++ src/components/UserMenu.jsx
@@ -26,7 +26,7 @@ function UserMenu({ desktop }) {
         <User className="w-5 h-5" /> <span>Profile</span>
       </div>
       <div
-        className="flex items-center gap- px-4 py-2 hover:bg-gray-200 dark:hover:bg-gray-700 rounded-lg cursor-pointer text-red-600"
+        className="flex items-center gap-2 px-4 py-2 hover:bg-gray-200 dark:hover:bg-gray-700 rounded-lg cursor-pointer text-red-600"
         onClick={handleLogout}
       >
         <LogOut className="w-5 h-5" /> <span>Logout</span>
